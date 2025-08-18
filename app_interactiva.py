f8e693cc1cbe2a6859915970f317e16ad67c3c5a
Tempestesscatt <simocorreos@gmail.com>
2025-08-18 16:04:11 +0200

    Update app_interactiva.py

--- app_interactiva.py
+++ app_interactiva.py
@@ -201,8 +201,8 @@ def crear_mapa_forecast_combinat(lons, lats, dewpoint_data, speed_data, dir_data
     ax.streamplot(
         grid_lon, grid_lat, grid_u, grid_v, 
         color='black', 
-        linewidth=0.3,      # Una mica més primes per compensar la densitat
-        density=5.9,        # Augmentem la densitat (valor anterior: 1.5)
+        linewidth=0.8,      # Una mica més primes per compensar la densitat
+        density=5.0,        # Augmentem la densitat (valor anterior: 1.5)
         arrowsize=0.3,      # Reduïm la mida de la fletxa (valor anterior: 0.7)
         zorder=4
     )
