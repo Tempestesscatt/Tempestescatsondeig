a6f70656d735bb781e3c5700ed8b72ebc9a3188c
Tempestesscatt <simocorreos@gmail.com>
2025-08-18 10:35:19 +0200

    Update app_interactiva.py

--- app_interactiva.py
+++ app_interactiva.py
@@ -25,7 +25,7 @@ cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
 retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
 openmeteo = openmeteo_requests.Client(session=retry_session)
 
-FORECAST_DAYS = 4
+FORECAST_DAYS = 2
 API_URL = "https://api.open-meteo.com/v1/forecast"
 TIMEZONE = pytz.timezone('Europe/Madrid')
 
