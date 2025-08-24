b1301bb4c923c1212acc87607c3727b2d515c576
Tempestesscatt <simocorreos@gmail.com>
2025-08-24 22:20:24 +0200

    Update app_interactiva.py

--- app_interactiva.py
+++ app_interactiva.py
@@ -354,7 +354,6 @@ def processar_dades_sondeig(p_profile, T_profile, Td_profile, u_profile, v_profi
         main_prof = ml_prof if ml_prof is not None else sfc_prof
 
         # --- 3. CÀLCULS ROBUSTS I AÏLLATS ---
-        # Aquests càlculs no depenen de la parcel·la
         try: params_calc['PWAT'] = float(mpcalc.precipitable_water(p, Td).to('mm').m)
         except: params_calc['PWAT'] = np.nan
         try:
@@ -366,41 +365,38 @@ def processar_dades_sondeig(p_profile, T_profile, Td_profile, u_profile, v_profi
         try: params_calc['LR_0-3km'] = float(mpcalc.lapse_rate(p, T, height=heights_agl, depth=3000 * units('m')).to('delta_degC/km').m)
         except: params_calc['LR_0-3km'] = np.nan
         
-        # Paràmetres de Superfície (SBCAPE)
-        try:
-            sbcape, sbcin = mpcalc.cape_cin(p, T, Td, sfc_prof)
-            params_calc['SBCAPE'] = float(sbcape.m); params_calc['SBCIN'] = float(sbcin.m)
-            params_calc['MAX_UPDRAFT'] = np.sqrt(2 * float(sbcape.m)) if sbcape.m > 0 else 0.0
-        except: params_calc.update({'SBCAPE': np.nan, 'SBCIN': np.nan, 'MAX_UPDRAFT': np.nan})
+        if sfc_prof is not None:
+            try:
+                sbcape, sbcin = mpcalc.cape_cin(p, T, Td, sfc_prof)
+                params_calc['SBCAPE'] = float(sbcape.m); params_calc['SBCIN'] = float(sbcin.m)
+                params_calc['MAX_UPDRAFT'] = np.sqrt(2 * float(sbcape.m)) if sbcape.m > 0 else 0.0
+            except: params_calc.update({'SBCAPE': np.nan, 'SBCIN': np.nan, 'MAX_UPDRAFT': np.nan})
         
-        # Paràmetres de Capa Barrejada (MLCAPE)
         if ml_prof is not None:
             try:
                 mlcape, mlcin = mpcalc.cape_cin(p, T, Td, ml_prof)
                 params_calc['MLCAPE'] = float(mlcape.m); params_calc['MLCIN'] = float(mlcin.m)
             except: params_calc.update({'MLCAPE': np.nan, 'MLCIN': np.nan})
-        else: # Si ml_prof no existeix, MLCAPE no es pot calcular
+        else:
             params_calc.update({'MLCAPE': np.nan, 'MLCIN': np.nan})
 
-        # Altres paràmetres termodinàmics, ara utilitzant 'main_prof'
-        try: params_calc['LI'] = float(mpcalc.lifted_index(p, T, main_prof).m)
-        except: params_calc['LI'] = np.nan
-        try:
-            lfc_p, _ = mpcalc.lfc(p, T, Td, main_prof); params_calc['LFC_p'] = float(lfc_p.m)
-            params_calc['LFC_Hgt'] = float(np.interp(lfc_p.m, p.m[::-1], heights_agl.m[::-1]))
-        except: params_calc.update({'LFC_p': np.nan, 'LFC_Hgt': np.nan})
-        try:
-            el_p, _ = mpcalc.el(p, T, Td, main_prof); params_calc['EL_p'] = float(el_p.m)
-            params_calc['EL_Hgt'] = float(np.interp(el_p.m, p.m[::-1], heights_agl.m[::-1]))
-        except: params_calc.update({'EL_p': np.nan, 'EL_Hgt': np.nan})
-        try:
-            idx_3km = np.argmin(np.abs(heights_agl.m - 3000))
-            cape_0_3, _ = mpcalc.cape_cin(p[:idx_3km+1], T[:idx_3km+1], Td[:idx_3km+1], main_prof[:idx_3km+1])
-            params_calc['CAPE_0-3km'] = float(cape_0_3.m)
-        except: params_calc['CAPE_0-3km'] = np.nan
+        if main_prof is not None:
+            try: params_calc['LI'] = float(mpcalc.lifted_index(p, T, main_prof).m)
+            except: params_calc['LI'] = np.nan
+            try:
+                lfc_p, _ = mpcalc.lfc(p, T, Td, main_prof); params_calc['LFC_p'] = float(lfc_p.m)
+                params_calc['LFC_Hgt'] = float(np.interp(lfc_p.m, p.m[::-1], heights_agl.m[::-1]))
+            except: params_calc.update({'LFC_p': np.nan, 'LFC_Hgt': np.nan})
+            try:
+                el_p, _ = mpcalc.el(p, T, Td, main_prof); params_calc['EL_p'] = float(el_p.m)
+                params_calc['EL_Hgt'] = float(np.interp(el_p.m, p.m[::-1], heights_agl.m[::-1]))
+            except: params_calc.update({'EL_p': np.nan, 'EL_Hgt': np.nan})
+            try:
+                idx_3km = np.argmin(np.abs(heights_agl.m - 3000))
+                cape_0_3, _ = mpcalc.cape_cin(p[:idx_3km+1], T[:idx_3km+1], Td[:idx_3km+1], main_prof[:idx_3km+1])
+                params_calc['CAPE_0-3km'] = float(cape_0_3.m)
+            except: params_calc['CAPE_0-3km'] = np.nan
         
-        # ... (la resta de la funció amb cinemàtica, STP, SCP, etc., es manté igual,
-        # ja que la seva robustesa ja estava implementada) ...
         try:
             mucape, mucin = mpcalc.most_unstable_cape_cin(p, T, Td); params_calc['MUCAPE'] = float(mucape.m)
             params_calc['MUCIN'] = float(mucin.m)
@@ -416,7 +412,7 @@ def processar_dades_sondeig(p_profile, T_profile, Td_profile, u_profile, v_profi
                 params_calc[f'BWD_{name}'] = float(mpcalc.wind_speed(bwd_u, bwd_v).to('kt').m)
         except: params_calc.update({'BWD_0-1km': np.nan, 'BWD_0-6km': np.nan})
         try:
-            rm, lm, _ = mpcalc.bunkers_storm_motion(p, u, v, heights)
+            rm, lm, _ = mpcalc.bunker_storm_motion(p, u, v, heights)
             params_calc['RM'] = (float(rm[0].m), float(rm[1].m)); params_calc['LM'] = (float(lm[0].m), float(lm[1].m))
         except: params_calc.update({'RM': (np.nan, np.nan), 'LM': (np.nan, np.nan)})
         
@@ -450,7 +446,6 @@ def processar_dades_sondeig(p_profile, T_profile, Td_profile, u_profile, v_profi
 
     # Retornem el perfil de superfície (sfc_prof) per al dibuix, que és el més robust
     return ((p, T, Td, u, v, heights, sfc_prof), params_calc), None
-
 
 
     
