9792188dce9488828184c4da9478dae43197477c
Tempestesscatt <simocorreos@gmail.com>
2025-08-25 10:48:12 +0200

    Update app_interactiva.py

--- app_interactiva.py
+++ app_interactiva.py
@@ -408,11 +408,11 @@ def calcular_mlcape_robusta(p, T, Td):
 
 def processar_dades_sondeig(p_profile, T_profile, Td_profile, u_profile, v_profile, h_profile):
     """
-    Versió Definitiva i Antifràgil v5.3.
-    Soluciona l'error del moviment de la tempesta capturant el vent mitjà
-    directament del càlcul de Bunkers per garantir la coherència.
+    Versió Definitiva i Antifràgil v5.4 - CORRECCIÓ LCL/LFC.
+    Utilitza la funció d'interpolació de MetPy (interpolate_1d) per a un càlcul
+    robust de les alçades LCL, LFC i EL, solucionant errors de NaN.
     """
-    # --- 1. PREPARACIÓ I NETEJA DE DADES ---
+    # --- 1. PREPARACIÓ I NETEJA DE DADES (Sense canvis) ---
     if len(p_profile) < 4: return None, "Perfil atmosfèric massa curt."
     p = np.array(p_profile) * units.hPa; T = np.array(T_profile) * units.degC
     Td = np.array(Td_profile) * units.degC; u = np.array(u_profile) * units('m/s')
@@ -424,7 +424,7 @@ def processar_dades_sondeig(p_profile, T_profile, Td_profile, u_profile, v_profi
     p, T, Td, u, v, heights = p[sort_idx], T[sort_idx], Td[sort_idx], u[sort_idx], v[sort_idx], heights[sort_idx]
     params_calc = {}; heights_agl = heights - heights[0]
 
-    # --- 2. CÀLCULS BASE I PERFILS DE PARCEL·LA ---
+    # --- 2. CÀLCULS BASE I PERFILS DE PARCEL·LA (Sense canvis) ---
     with parcel_lock:
         sfc_prof, ml_prof = None, None
         try: sfc_prof = mpcalc.parcel_profile(p, T[0], Td[0]).to('degC')
@@ -434,7 +434,7 @@ def processar_dades_sondeig(p_profile, T_profile, Td_profile, u_profile, v_profi
         main_prof = ml_prof if ml_prof is not None else sfc_prof
 
         # --- 3. CÀLCULS ROBUSTS I AÏLLATS ---
-        # (Aquí va tota la secció de càlculs de PWAT, LI, T_500hPa, CAPE, etc. que ja tenies)
+        # (La majoria de càlculs es mantenen igual)
         try: 
             rh = mpcalc.relative_humidity_from_dewpoint(T, Td) * 100
             params_calc['RH_CAPES'] = {'baixa': np.mean(rh[(p.m <= 1000) & (p.m > 850)]), 'mitjana': np.mean(rh[(p.m <= 850) & (p.m > 500)]), 'alta': np.mean(rh[(p.m <= 500) & (p.m > 250)])}
@@ -461,47 +461,53 @@ def processar_dades_sondeig(p_profile, T_profile, Td_profile, u_profile, v_profi
                 mlcape, mlcin = mpcalc.cape_cin(p, T, Td, ml_prof); params_calc['MLCAPE'] = float(mlcape.m); params_calc['MLCIN'] = float(mlcin.m)
             except: params_calc.update({'MLCAPE': np.nan, 'MLCIN': np.nan})
         else: params_calc.update({'MLCAPE': np.nan, 'MLCIN': np.nan})
+        
+        # --- BLOC DE L'LFC, LCL i EL TOTALMENT CORREGIT ---
         if main_prof is not None:
             try: params_calc['LI'] = float(mpcalc.lifted_index(p, T, main_prof).m)
             except: params_calc['LI'] = np.nan
             try:
                 lfc_p, _ = mpcalc.lfc(p, T, Td, main_prof); params_calc['LFC_p'] = float(lfc_p.m)
-                params_calc['LFC_Hgt'] = float(np.interp(lfc_p.m, p.m[::-1], heights_agl.m[::-1]))
+                # --- CANVI CLAU: Utilitzem la interpolació de MetPy ---
+                lfc_hgt_interpolated = mpcalc.interpolate_1d(lfc_p, p, heights_agl)
+                params_calc['LFC_Hgt'] = float(lfc_hgt_interpolated.m)
             except: params_calc.update({'LFC_p': np.nan, 'LFC_Hgt': np.nan})
             try:
                 el_p, _ = mpcalc.el(p, T, Td, main_prof); params_calc['EL_p'] = float(el_p.m)
-                params_calc['EL_Hgt'] = float(np.interp(el_p.m, p.m[::-1], heights_agl.m[::-1]))
+                # --- CANVI CLAU: Utilitzem la interpolació de MetPy ---
+                el_hgt_interpolated = mpcalc.interpolate_1d(el_p, p, heights_agl)
+                params_calc['EL_Hgt'] = float(el_hgt_interpolated.m)
             except: params_calc.update({'EL_p': np.nan, 'EL_Hgt': np.nan})
             try:
                 idx_3km = np.argmin(np.abs(heights_agl.m - 3000))
                 cape_0_3, _ = mpcalc.cape_cin(p[:idx_3km+1], T[:idx_3km+1], Td[:idx_3km+1], main_prof[:idx_3km+1]); params_calc['CAPE_0-3km'] = float(cape_0_3.m)
             except: params_calc['CAPE_0-3km'] = np.nan
+        
         try:
             mucape, mucin = mpcalc.most_unstable_cape_cin(p, T, Td); params_calc['MUCAPE'] = float(mucape.m); params_calc['MUCIN'] = float(mucin.m)
         except: params_calc.update({'MUCAPE': np.nan, 'MUCIN': np.nan})
+        
         try:
-            lcl_p, _ = mpcalc.lcl(p[0], T[0], Td[0]); params_calc['LCL_p'] = float(lcl_p.m); params_calc['LCL_Hgt'] = float(np.interp(lcl_p.m, p.m[::-1], heights_agl.m[::-1]))
+            lcl_p, _ = mpcalc.lcl(p[0], T[0], Td[0]); params_calc['LCL_p'] = float(lcl_p.m)
+            # --- CANVI CLAU: Utilitzem la interpolació de MetPy ---
+            lcl_hgt_interpolated = mpcalc.interpolate_1d(lcl_p, p, heights_agl)
+            params_calc['LCL_Hgt'] = float(lcl_hgt_interpolated.m)
         except: params_calc.update({'LCL_p': np.nan, 'LCL_Hgt': np.nan})
+        
+        # --- FI DEL BLOC CORREGIT ---
+        
         try:
             for name, depth_m in [('0-1km', 1000), ('0-6km', 6000)]:
                 bwd_u, bwd_v = mpcalc.bulk_shear(p, u, v, height=heights, depth=depth_m * units.meter); params_calc[f'BWD_{name}'] = float(mpcalc.wind_speed(bwd_u, bwd_v).to('kt').m)
         except: params_calc.update({'BWD_0-1km': np.nan, 'BWD_0-6km': np.nan})
         
-        # *** LÒGICA DE MOVIMENT DE TEMPESTA CORREGIDA ***
         try:
-            # Aquesta funció retorna el moviment dret, esquerre I el vent mitjà.
-            # Ara capturem els tres resultats correctament.
             rm, lm, mean_wind = mpcalc.bunkers_storm_motion(p, u, v, heights)
             params_calc['RM'] = (float(rm[0].m), float(rm[1].m))
             params_calc['LM'] = (float(lm[0].m), float(lm[1].m))
             params_calc['Mean_Wind'] = (float(mean_wind[0].m), float(mean_wind[1].m))
         except Exception:
-            # Si el càlcul falla, assegurem que tots tres siguin N/A.
-            params_calc.update({
-                'RM': (np.nan, np.nan),
-                'LM': (np.nan, np.nan),
-                'Mean_Wind': (np.nan, np.nan)
-            })
+            params_calc.update({'RM': (np.nan, np.nan), 'LM': (np.nan, np.nan), 'Mean_Wind': (np.nan, np.nan)})
 
         if params_calc.get('RM') and not np.isnan(params_calc['RM'][0]):
             u_storm, v_storm = params_calc['RM'][0] * units('m/s'), params_calc['RM'][1] * units('m/s')
